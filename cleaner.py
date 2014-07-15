b45127ed05fb081f1feb5ffa73bc5bca42577c3d
Torf <tberger.dev@gmail.com>
2014-07-15 14:32:54 +0200

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -2,8 +2,16 @@ import discogs_client as discogs
 
 discogs.user_agent = 'Pytest/0.1 +http://ww.abc.fr'
 
-s = discogs.NewSearch('azealia banks', 'artist')
+def searchArtistName(foldername):
+  s = discogs.NewSearch(foldername, 'artist')
+  result = s.results()
+  
+  if len(result) >= 1:
+    return result[0].name
+    
+  return None
 
-print s._uri
 
-print s.results()
+print searchArtistName('azealia banks')
+
+
