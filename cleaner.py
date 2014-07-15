d96b0f7f79b650ad4b2ff9d7eb7a7366d3b29351
Torf <tberger.dev@gmail.com>
2014-07-15 14:19:55 +0200

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -2,7 +2,7 @@ import discogs_client as discogs
 
 discogs.user_agent = 'Pytest/0.1 +http://ww.abc.fr'
 
-s = discogs.NewSearch('azealia banks')
+s = discogs.NewSearch('azealia banks', 'artist')
 
 print s._uri
 
