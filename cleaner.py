7431aa3b19b182ec4ddf2bb471b072fe4ac20b7a
Torf <tberger.dev@gmail.com>
2014-07-15 13:54:29 +0200

    Update cleaner.py

--- cleaner.py
+++ cleaner.py
@@ -2,7 +2,7 @@ import discogs_client as discogs
 
 discogs.user_agent = 'Pytest/0.1 +http://ww.abc.fr'
 
-s = discogs.Search('azealia banks')
+s = discogs.NewSearch('azealia banks')
 
 print s
 print s.exactresults
