1f635c776e54887c89e01fd553f2c3c707efb1ef
Torf <tberger.dev@gmail.com>
2014-07-16 01:56:42 +0200

    Update folders.py

--- folders.py
+++ folders.py
@@ -1,15 +1,26 @@
 
 class BaseFolder(object):
 
-	def __init__(self, folderName):
+	def __init__(self, musicFolder, folderName):
+		self._musicFolderPath = musicFolder
+		if not self._musicFolderPath.endswith("/"):
+     		self._musicFolderPath += "/"
+
         self._name = folderName
 
     def __str__(self):
-        return '<%s "%s">' % (self.__class__.__name__, self._name)
+        return '<%s "%s">' % (self.__class__.__name__, self.name)
 
     def __repr__(self):
         return self.__str__().encode('utf-8')
 
+	@property
+	def uri():
+		return "%s%s" % (self._musicFolderPath, self._name)
+
+	@property
+	def name():
+		return self._name
 
 class ArtistFolder(BaseFolder):
 
@@ -24,5 +35,8 @@ class AlbumFolder(BaseFolder):
 		BaseFolder.__init__(self, folderName)
 
 	def __str__(self):
-        return '<%s "%s (%s)">' % (self.__class__.__name__, self._name, self._artistFolder)
-    
+        return '<%s "%s (%s)">' % (self.__class__.__name__, self._name, self._artistFolder.name)
+
+     @property
+     def uri():
+     	return "%s%s/%s" % (self._musicFolderPath, artistFolder.name, self._name)
